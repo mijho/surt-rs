570a1ffbc4f429a3b2d82b9c682ab7f00a4588b1
mijho <mark@mijho.co>
2024-03-23 13:00:22 +0000

    chore: fmt changes

--- src/bin/surt.rs
+++ src/bin/surt.rs
@@ -1,12 +1,11 @@
-use url::{ParseError};
 use std::env;
-use surt_rs::{generate_surt};
-
+use surt_rs::generate_surt;
+use url::ParseError;
 
 // This will generate the SURT for the given URL
 fn main() -> Result<(), ParseError> {
     let args: Vec<String> = env::args().collect();
-    let url = &args[1]; 
+    let url = &args[1];
     let surt = generate_surt(url).unwrap_or_else(|_| url.to_string());
 
     println!("{}", surt);
